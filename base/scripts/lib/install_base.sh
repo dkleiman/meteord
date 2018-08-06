25f8e2aefa92fc0048e8dcf595dbcab3e59745cb
dkleiman <dave.kleiman2@gmail.com>
2018-08-06 16:15:53 -0400

    Install missing dependencies for node canvas

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -2,3 +2,8 @@
 set -e
 apt-get update -y
 apt-get install -y curl bzip2 build-essential g++ python git
+
+add-apt-repository -y ppa:glasen/freetype2
+apt install freetype2-demos
+
+apt-get install -y libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev
